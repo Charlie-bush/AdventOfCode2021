5f7708385ed0aa5b7201a5c3b3eb93113ef24863
charles.bush <charles.bush@fdmgroup.com>
2021-12-03 17:34:19 +0000

    Check commits working properly

---
+++ src/main/com/week1/day4/DayFour.java
@@ -0,0 +1,17 @@
+package main.com.week1.day4;
+
+import main.com.AdventUtils.AdventDay;
+
+public class DayFour implements AdventDay {
+    private String result;
+
+    @Override
+    public void process() {
+
+    }
+
+    @Override
+    public String getResult() {
+        return result;
+    }
+}
